6d7f4cfdd0d026d3142274a8f2e698514d153b9c
Carlo Ledesma <carlomiguel.ledesma@misys.com>
2018-07-24 08:05:40 +0800

    tf

--- node/neuralnet/lib/NeuralNetworkTF.js
+++ node/neuralnet/lib/NeuralNetworkTF.js
@@ -1,10 +1,10 @@
+let tf = require('@tensorflow/tfjs');
+// Load the binding
+require('@tensorflow/tfjs-node');
+
 class NeuralNetworkTF {
 
     constructor(inputs, hidden, outputs, lr) {
-
-        let tf = require('@tensorflow/tfjs');
-        // Load the binding
-        require('@tensorflow/tfjs-node');
 
         this.model = tf.sequential();
         tf.tensor([[[1, 2], [3, 4]]]).print();
