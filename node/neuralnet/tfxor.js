90cae98200b32b3b1fa87f13eb664726c787d967
Carlo Ledesma <carlomiguel.ledesma@misys.com>
2018-07-24 16:53:36 +0800

    tf

--- node/neuralnet/tfxor.js
+++ node/neuralnet/tfxor.js
@@ -1,4 +1,56 @@
+tf = require('@tensorflow/tfjs');
+require('@tensorflow/tfjs-node'); // Load the binding
+
 module.exports = function () {
-    NeuralNetworkTF = require('./lib/NeuralNetworkTF.js');
-    nn = new NeuralNetworkTF();
+
+    const model = tf.sequential();
+    model.add(tf.layers.dense({
+        inputShape: 2,
+        units: 4,
+        activation: 'sigmoid'
+    }));
+    model.add(tf.layers.dense({
+        units: 1,
+        activation: 'sigmoid'
+    }));
+    const learningRate = 0.1;
+    model.compile({ 
+        optimizer: tf.train.sgd(learningRate),
+        loss: tf.losses.meanSquaredError
+    });
+
+    // train
+
+    const xs = tf.tensor2d([
+        [1,1],
+        [0,0],
+        [0,1],
+        [1,0]
+    ]);
+
+    const ys = tf.tensor2d([
+        [0],
+        [0],
+        [1],
+        [1]
+    ]);
+
+    async function train() {
+        for (let i = 0; i < 10000; i++) {
+            const response = await model.fit(xs, ys);
+            console.log(response.history.loss[0]);
+        }
+    }
+
+    train().then(() => {
+        const inputs = tf.tensor2d([
+            [1,1],
+            [0,0],
+            [0,1],
+            [1,0]
+        ]);
+        let outputs = model.predict(inputs);
+        outputs.print();
+    });
+
 };
