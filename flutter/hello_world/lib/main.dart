9b4a27ad02999fa7c2e2d74b3f9ff1a23bdc6652
Carlo Ledesma <carlomiguel.ledesma@misys.com>
2018-07-26 09:55:36 +0800

    list

--- flutter/hello_world/lib/main.dart
+++ flutter/hello_world/lib/main.dart
@@ -6,14 +6,12 @@ void main() {
 }
 
 class MainApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp (
-      title: 'Startup Name Generator',
-      home: RandomWords()
-    );
+    return MaterialApp(
+        title: 'Startup Name Generator',
+        theme: ThemeData(primaryColor: Colors.white),
+        home: RandomWords());
   }
 }
 
@@ -22,41 +20,74 @@ class RandomWords extends StatefulWidget {
   createState() => RandomWordsState();
 }
 
-class RandomWordsState extends State<RandomWords>{
-
+class RandomWordsState extends State<RandomWords> {
   final suggestions = <WordPair>[];
   final biggerFont = TextStyle(fontSize: 18.0);
+  final saved = Set<WordPair>();
+
+  void pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (context) {
+        final tiles = saved.map((pair) {
+          return ListTile(
+            title: Text(pair.asPascalCase, style: biggerFont),
+          );
+        });
+        final divided = ListTile
+            .divideTiles(
+              context: context,
+              tiles: tiles,
+            )
+            .toList();
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Saved Suggestions'),
+          ),
+          body: ListView(children: divided),
+        );
+      }),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Startup Name Generator'),
-      ),
-      body: buildSuggestions()
-    );
+        appBar: AppBar(title: Text('Startup Name Generator'), actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: pushSaved)
+        ]),
+        body: buildSuggestions());
   }
 
   Widget buildSuggestions() {
     return ListView.builder(
-      padding: EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return Divider();
-        final index = i ~/ 2;
-        if (index >= suggestions.length) {
-          suggestions.addAll(generateWordPairs().take(10));
-        }
-        return buildRow(suggestions[index]);
-      }
-    );
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+          final index = i ~/ 2;
+          if (index >= suggestions.length) {
+            suggestions.addAll(generateWordPairs().take(10));
+          }
+          return buildRow(suggestions[index]);
+        });
   }
 
   Widget buildRow(WordPair pair) {
+    final alreadySaved = saved.contains(pair);
     return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: biggerFont,
-      )
-    );
+        title: Text(
+          pair.asPascalCase,
+          style: biggerFont,
+        ),
+        trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              saved.remove(pair);
+            } else {
+              saved.add(pair);
+            }
+          });
+        });
   }
 }
