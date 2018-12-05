9d594b18f9b2945872f845d43a014b8449625f44
Carlo Ledesma <carlomiguel.ledesma@finastra.com>
2018-12-05 10:05:36 +0800

    verify

--- python/veritrade/functions/hello_func/main.py
+++ python/veritrade/functions/hello_func/main.py
@@ -1,2 +1,13 @@
-def hello_func(request):
+def verify(request):
     return "Hello func!"
+
+def detect_labels():
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+    # TODO Define content from Base64
+    image = vision.types.Image(content=content)
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    print('Labels:')
+    for label in labels:
+        print(label.description)
