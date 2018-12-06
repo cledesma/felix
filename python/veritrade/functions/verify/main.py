599859a4497975ef81a5409a1c8c5811785861dd
Ledesma, Carlo Miguel <CarloMiguel.Ledesma@misys.com>
2018-12-06 21:17:00 +0800

    Checkpoint commit

--- python/veritrade/functions/verify/main.py
+++ python/veritrade/functions/verify/main.py
@@ -28,20 +28,20 @@ def detect_entities(goods_declaration):
     document = {'type':  enums.Document.Type.PLAIN_TEXT, 'content': content}
 
     entities = client.analyze_entities(document).entities
-    entity_type = ('UNKNOWN', 'PERSON', 'LOCATION', 'ORGANIZATION', 
-        'EVENT', 'WORK_OF_ART', 'CONSUMER_GOOD', 'OTHER')
+    # entity_type = ('UNKNOWN', 'PERSON', 'LOCATION', 'ORGANIZATION', 
+    #     'EVENT', 'WORK_OF_ART', 'CONSUMER_GOOD', 'OTHER')
     print('\n')
     print('=' * 20)
     print("Entities")
     print('=' * 20)
     for entity in entities:
-        print('=' * 20)
         print(u'{:<16}: {}'.format('name', entity.name))
-        print(u'{:<16}: {}'.format('type', entity_type[entity.type]))
-        print(u'{:<16}: {}'.format('metadata', entity.metadata))
         print(u'{:<16}: {}'.format('salience', entity.salience))
-        print(u'{:<16}: {}'.format('wikipedia_url',
-              entity.metadata.get('wikipedia_url', '-')))
+        # print(u'{:<16}: {}'.format('type', entity_type[entity.type]))
+        # print(u'{:<16}: {}'.format('metadata', entity.metadata))
+        # print(u'{:<16}: {}'.format('wikipedia_url',
+        #       entity.metadata.get('wikipedia_url', '-')))
+    return entities
 
 def detect_labels(goods_image_uri):
 
@@ -57,17 +57,48 @@ def detect_labels(goods_image_uri):
     for label in labels:
         print(label.description)
         print(label.score)
+    return labels
 
-def detect_texts(document_image_uri):
+def detect_image_texts(image_uri):
+
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+    image = vision.types.Image()
+    image.source.image_uri = image_uri
+    texts = client.text_detection(image=image).text_annotations
+    print("\n")
+    print('=' * 20)
+    print("Image Text")
+    print('=' * 20)
+    for text in texts:
+        print(text.description)
+    return texts
+
+def detect_document_texts(document_image_uri):
 
     from google.cloud import vision
     client = vision.ImageAnnotatorClient()
     image = vision.types.Image()
     image.source.image_uri = document_image_uri
-    texts = client.text_detection(image=image).text_annotations
+    response = client.document_text_detection(image=image)
+    paragraphs = []
+    for page in response.full_text_annotation.pages:
+        for block in page.blocks:
+            for paragraph in block.paragraphs:
+                par = ""
+                for word in paragraph.words:
+                    word_text = ''.join([
+                        symbol.text for symbol in word.symbols
+                    ])
+                    par += word_text + " "
+                paragraphs.append(par)
+    all_text = ""
+    for paragraph in paragraphs:
+        all_text += paragraph + "\n"
     print("\n")
     print('=' * 20)
-    print("Document Text")
+    print("All Document Text")
     print('=' * 20)
-    for text in texts:
-        print(text.description)
+    print(all_text )
+    entities = detect_entities(all_text)
+    return entities
--- python/veritrade/functions/verify/main_test.py
+++ python/veritrade/functions/verify/main_test.py
@@ -5,11 +5,15 @@ import main
 # def test_verify():
 #     pass
 
-def test_detect_entities():
-    main.detect_entities("A slice of bacon is better than all the salt in the world.")
+# def test_detect_entities():
+    # entities = main.detect_entities("20 bags of horse manure")
 
-def test_detect_labels():
-    main.detect_labels('gs://veritrade/car.jpg')
+# def test_detect_labels():
+#     main.detect_labels('gs://veritrade/lubricants.jpg')
 
-def test_detect_texts():
-    main.detect_texts('gs://veritrade/Bill-of-Lading.jpg')
+# def test_detect_image_texts():
+#     main.detect_image_texts('gs://veritrade/Bill-of-Lading.jpg')
+
+def test_detect_document_texts():
+    document_texts = main.detect_document_texts('gs://veritrade/original_bill_of_loading.jpg')
+    
