190b95dfe4b425b35d7f54175da9b091c11afa5f
Ledesma, Carlo Miguel <CarloMiguel.Ledesma@misys.com>
2018-12-06 11:33:14 +0800

    Checkpoint commit

--- python/veritrade/functions/verify/main_test.py
+++ python/veritrade/functions/verify/main_test.py
@@ -2,3 +2,14 @@ from unittest.mock import Mock
 
 import main
 
+def test_verify():
+    pass
+
+def test_detect_entities():
+    pass
+
+def test_detect_labels():
+    main.detect_labels('gs://veritrade/dog.jpg')
+
+def test_detect_texts():
+    pass
